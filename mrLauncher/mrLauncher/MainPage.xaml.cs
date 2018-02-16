f73dccc2c6af5b74b44422914b8419a7bea5ed61
Josh Keenan <joshk42@users.noreply.github.com>
2018-02-16 12:36:38 -0800

    Add Frist Release

---
+++ mrLauncher/mrLauncher/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace mrLauncher
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Done(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("steam://rungameid/250820"));
+            Environment.Exit(0);
+        }
+    }
+}
